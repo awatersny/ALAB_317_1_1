30992e0e875dae9a1044dde7e9829d8346544c2c
awatersny <awatersny@gmail.com>
2025-04-02 11:46:54 -0400

    Add printAll function and modify print function according to provided tests

--- lab.ts
+++ lab.ts
@@ -33,11 +33,11 @@ class MotorCycle<T> extends Vehicle<T> {
 
 class NCycle<T> {
   status: string = "stopped";
-  make: string | string[]
-  model: string | string[]
+  make: T | T[]
+  model: T | T[]
   wheels: number
 
-  constructor(make: string | string[], model: string | string[], wheels: number) {
+  constructor(make: T | T[], model: T | T[], wheels: number) {
     this.make = make;
     this.model = model;
     this.wheels = wheels;
@@ -49,15 +49,28 @@ class NCycle<T> {
     this.status = "stopped";
   }
   print(index: number = 0) {
-    if(typeof this.make === "string" && typeof this.model === "string"){
-      console.log(`"This is a ${this.make} ${this.model} NCycle."`)
-    } 
-    else if(this.make.length > index && this.model.length > index){
-      console.log(`"This NCycle has a ${this.make} ${this.model} at ${index}."`)
+    if(Array.isArray(this.make) && Array.isArray(this.model)){
+      if(this.make.length > index && this.model.length > index){
+        console.log(`This NCycle has a ${this.make} ${this.model} at ${index}.`)
+      }
+      else {
+        console.log("This NCycle was not created properly.")
+      }
     } 
     else {
-      console.log("This NCycle was not created properly.")
-    }
+      console.log(`This is a ${this.make} ${this.model} NCycle.`)
+    } 
+  }
+  printAll() {
+    if(Array.isArray(this.make) && Array.isArray(this.model)){
+      const shorter: T | T[] = this.make.length < this.model.length ? this.make : this.model
+      for(let i:number = 0; i < shorter.length; i++) {
+        console.log(`This NCycle has a ${this.make} ${this.model} at ${i}.`)
+      }
+    } 
+    else {
+      console.log(`"This is a ${this.make} ${this.model} NCycle."`)
+    } 
   }
 }
 
@@ -78,3 +91,35 @@ const myBuick = new Car<string>("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
 console.log(myBuick.model);
+
+// Rudimentary testing Code, not part of the assignment
+const testCycle1 = new NCycle<number>(1, 2, 3);
+testCycle1.print();
+testCycle1.printAll();
+
+const testCycle2 = new NCycle<string>("This", "That", 4);
+testCycle2.print();
+testCycle2.printAll();
+
+const testCycle3 = new NCycle<string | number>("Make", 10, 4);
+testCycle3.print(4);
+testCycle3.printAll();
+
+const makes4 = ["Volkswagon", "Tesla", "Audi"];
+const models4 = ["Passat", "Model X", "A4"];
+const testCycle4 = new NCycle<string[]>(makes4, models4, 4);
+testCycle4.print(2);
+testCycle4.printAll();
+
+const makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const models5 = [1, 1, 2, 3, 5];
+const testCycle5 = new NCycle<number[]>(makes5, models5, 0);
+testCycle5.print(7);
+testCycle5.printAll();
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+// add(testCycle1.make, testCycle5.model[1]);
+// // Error expected here
+// add(testCycle2.make, testCycle4.model[1]);
--- out/lab.js
+++ out/lab.js
@@ -23,6 +23,44 @@ class MotorCycle extends Vehicle {
         super(make, model, 2);
     }
 }
+class NCycle {
+    constructor(make, model, wheels) {
+        this.status = "stopped";
+        this.make = make;
+        this.model = model;
+        this.wheels = wheels;
+    }
+    start() {
+        this.status = "started";
+    }
+    stop() {
+        this.status = "stopped";
+    }
+    print(index = 0) {
+        if (Array.isArray(this.make) && Array.isArray(this.model)) {
+            if (this.make.length > index && this.model.length > index) {
+                console.log(`This NCycle has a ${this.make} ${this.model} at ${index}.`);
+            }
+            else {
+                console.log("This NCycle was not created properly.");
+            }
+        }
+        else {
+            console.log(`This is a ${this.make} ${this.model} NCycle.`);
+        }
+    }
+    printAll() {
+        if (Array.isArray(this.make) && Array.isArray(this.model)) {
+            const shorter = this.make.length < this.model.length ? this.make : this.model;
+            for (let i = 0; i < shorter.length; i++) {
+                console.log(`This NCycle has a ${this.make} ${this.model} at ${i}.`);
+            }
+        }
+        else {
+            console.log(`"This is a ${this.make} ${this.model} NCycle."`);
+        }
+    }
+}
 function printStatus(vehicle) {
     if (vehicle.status === "running") {
         console.log("The vehicle is running.");
@@ -39,3 +77,29 @@ const myBuick = new Car("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
 console.log(myBuick.model);
+// Rudimentary testing Code, not part of the assignment
+const testCycle1 = new NCycle(1, 2, 3);
+testCycle1.print();
+testCycle1.printAll();
+const testCycle2 = new NCycle("This", "That", 4);
+testCycle2.print();
+testCycle2.printAll();
+const testCycle3 = new NCycle("Make", 10, 4);
+testCycle3.print(4);
+testCycle3.printAll();
+const makes4 = ["Volkswagon", "Tesla", "Audi"];
+const models4 = ["Passat", "Model X", "A4"];
+const testCycle4 = new NCycle(makes4, models4, 4);
+testCycle4.print(2);
+testCycle4.printAll();
+const makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const models5 = [1, 1, 2, 3, 5];
+const testCycle5 = new NCycle(makes5, models5, 0);
+testCycle5.print(7);
+testCycle5.printAll();
+function add(x, y) {
+    return x + y;
+}
+// add(testCycle1.make, testCycle5.model[1]);
+// // Error expected here
+// add(testCycle2.make, testCycle4.model[1]);
